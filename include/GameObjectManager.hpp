9118aa3e216626aaabeebb42632470a4af70fc9c
Patrick RUSSELL <prussell@e4r5p2.wethinkcode.co.za>
2018-08-24 09:47:43 +0200

    Creation of GameObjectManager

---
+++ include/GameObjectManager.hpp
@@ -0,0 +1,36 @@
+//
+// Created by Patrick RUSSELL on 2018/08/24.
+//
+
+#ifndef BOMBERMAN_GAMEOBJECTMANAGER_HPP
+#define BOMBERMAN_GAMEOBJECTMANAGER_HPP
+
+#include "VisibleGameObject.hpp"
+
+class GameObjectManager
+{
+public:
+	GameObjectManager();
+	~GameObjectManager();
+
+	void add(std::string name, VisibleGameObject * gameObject);
+	void remove(const std::string & name);
+	int getObjectCount() const;
+	VisibleGameObject * get(const std::string & name) const;
+	void drawAll(sf::renderWindow & window);
+
+private:
+	std::map<std::string, VisibleGameObject*> _gameObjects;
+
+	//TODO remove implementation of GameObjectDeallocator to cpp
+	struct GameObjectDeallocator
+	{
+		void operator()(const std::pair<std::string, VisibleGameObject *> & p) const
+		{
+			delete p.second;
+		}
+	};
+
+};
+
+#endif //BOMBERMAN_GAMEOBJECTMANAGER_HPP
---
+++ src/GameObjectManager.cpp
@@ -0,0 +1,53 @@
+//
+// Created by Patrick RUSSELL on 2018/08/24.
+//
+
+#include "GameObjectManager.hpp"
+
+
+GameObjectManager::GameObjectManager() {}
+
+GameObjectManager::~GameObjectManager()
+{
+	std::for_each(_gameObjects.begin(), _gameObjects.end(), GameObjectDeallocator());
+}
+
+void GameObjectManager::add(std::string name, VisibleGameObject * gameObject)
+{
+	_gameObjects.insert(std::make_pair(name, gameObject));
+}
+
+void GameObjectManager::remove(const std::string & name)
+{
+	std::map<std::string, VisibleGameObject *>::iterator result = _gameObjects.find(name);
+	if(result != _gameObjects.end())
+	{
+		delete result->second;
+		_gameObjects.erase(result);
+	}
+}
+
+int GameObjectManager::getObjectCount() const
+{
+	return _gameObjects.size();
+}
+
+VisibleGameObject * GameObjectManager::get(const std::string & name) const
+{
+	std::map<std::string, VisibleGameObject *>::iterator result = _gameObjects.find(name);
+	if (result != _gameObjects.end())
+		return result->second;
+	return NULL;
+}
+
+void GameObjectManager::drawAll(sf::renderWindow & window)
+{
+	std::map<std::string, VisibleGameObject *>::const_iterator iter = _gameObjects.begin();
+	while(iter != _gameObjects.end())
+	{
+		iter->second->Draw(window);
+		iter++;
+	}
+}
+
+
