79b7aa4fcde02fef0e86868daa07f925dd086fb6
Patrick RUSSELL <prussell@e4r5p2.wethinkcode.co.za>
2018-09-04 12:32:30 +0200

    Object factory

--- src/ObjectFactory.cpp
+++ src/ObjectFactory.cpp
@@ -5,6 +5,30 @@
 #include "ObjectFactory.hpp"
 #include "Map"
 
+ObjectFactory::~ObjectFactory()
+{
+	delete _player;
+	delete _unbreakableBlock;
+	delete _breakableBlock;
+	delete _goomba;
+	delete _koopaTroopa;
+	delete _powerBlock;
+	delete _healthBlock;
+}
+
+ObjectFactory::ObjectFactory()
+{
+	_player = new Model("../assets/objects/mario_walking_1.png");
+	_unbreakableBlock = new Model("assets/objects/iron_block.png");
+	_breakableBlock = new Model("assets/objects/brick_block.png");
+	_goomba = new Model("assets/objects/goomba.obj");
+	_koopaTroopa = new Model("assets/objects/koopa_troopa.obj");
+	_powerBlock = new Model("assets/objects/brick_block.png");
+	_healthBlock = new Model("assets/objects/brick_block.png");
+
+
+}
+
 std::vector<std::vector<VisibleGameObject *>> * ObjectFactory::genStaticObjects()
 {
 	auto result = new std::vector<std::vector<VisibleGameObject * >>;
