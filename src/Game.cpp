bdc327bf59e82b4ea19184ccae0a017c3a6117ac
P-Russell <pdrussell011@gmail.com>
2018-09-25 14:28:50 +0200

    removes Screen/credits.hpp include from Game to get prog to compile

--- src/Game.cpp
+++ src/Game.cpp
@@ -27,7 +27,7 @@
 #include "Screens/GameOver.hpp"
 #include "Screens/GameWon.hpp"
 #include "Screens/LevelPassed.hpp"
-#include "Screens/Credits.hpp"
+//#include "Screens/Credits.hpp"
 
 Game::Game() {}
 
